b51c80a0dea26d257a7d5b43f44ada10413367ab
taiga-bocchi <121355764+taiga-bocchi@users.noreply.github.com>
2022-12-25 03:24:34 +0900

    処理速度向上させました。

--- chinchiro.py
+++ chinchiro.py
@@ -1,14 +1,13 @@
 import random
 
-cheating_flag = 1
-
-saikoro1 = random.randint(1,6)
-saikoro2 = random.randint(1,6)
-saikoro3 = random.randint(1,6)
-
-if cheating_flag == 1:
+cheating_flag = True
+if cheating_flag:
   saikoro1 = random.randint(4,6)
   saikoro2 = random.randint(4,6)
   saikoro3 = random.randint(4,6)
+else:
+    saikoro1 = random.randint(1,6)
+    saikoro2 = random.randint(1,6)
+    saikoro3 = random.randint(1,6)
 
 saikoro1, saikoro2, saikoro3
