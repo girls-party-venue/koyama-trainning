052c9c51e8eb6620e029e9c0c8dafbf7da4140b5
taiga-bocchi <121355764+taiga-bocchi@users.noreply.github.com>
2022-12-25 03:05:36 +0900

    ランダムでサイコロの数値を出力する機能を作成しました。

---
+++ chinchiro.py
@@ -0,0 +1,14 @@
+import random
+
+xxx = 1
+
+saikoro1 = random.randint(1,6)
+saikoro2 = random.randint(1,6)
+saikoro3 = random.randint(1,6)
+
+if xxx == 1:
+  saikoro1 = random.randint(4,6)
+  saikoro2 = random.randint(4,6)
+  saikoro3 = random.randint(4,6)
+
+saikoro1, saikoro2, saikoro3
