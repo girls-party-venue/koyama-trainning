76ff42fe69ac3fe69040dd5209b6eea8a7379b57
taiga-bocchi <121355764+taiga-bocchi@users.noreply.github.com>
2022-12-25 03:12:25 +0900

    修正したンゴ

--- chinchiro.py
+++ chinchiro.py
@@ -1,12 +1,12 @@
 import random
 
-xxx = 1
+cheating_flag = 1
 
 saikoro1 = random.randint(1,6)
 saikoro2 = random.randint(1,6)
 saikoro3 = random.randint(1,6)
 
-if xxx == 1:
+if cheating_flag == 1:
   saikoro1 = random.randint(4,6)
   saikoro2 = random.randint(4,6)
   saikoro3 = random.randint(4,6)
